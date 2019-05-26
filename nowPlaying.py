e2dc0bdbc1f940277f00a440b42bb58cec2486ca
father.luka@gmail.com <mihica.909>
2019-05-26 21:18:42 +0200

    MPV

---
+++ nowPlaying.py
@@ -0,0 +1,22 @@
+import os
+
+
+def nowPlaying():
+    with open('mpvLog.txt', 'r') as f:
+        lines = f.read().splitlines()
+        if 'Playing:' in lines[-3]:
+            np = lines[-3]
+        elif 'Playing:' in lines[-4]:
+            np = lines[-4]
+        elif 'Playing:' in lines[-9]:
+            np = lines[-9]
+        else:
+            np = lines[-5]
+        np = (np[9:])
+        np = (os.path.basename(np).split('.')[0])
+        print(np)
+    with  open('nowPlaying.txt', 'w') as f:
+        f.write(np)
+
+
+nowPlaying()
--- player.py
+++ player.py
@@ -1,8 +1,13 @@
 import time
 from OBS import obsSceneTransition, obsSceneVLC
 from subprocess import call
+import os
+
 
 def mpvPlayer():
-    call('mpv --quiet --gapless-audio --playlist playlistWithCommercials.pls', shell=True)
+    call('mpv --quiet --gapless-audio --write-filename-in-watch-later-config  >> mpvLog.txt --playlist playlistWithCommercials.pls', shell=True)
+
+
+
 
 mpvPlayer()
